e462afb77635103df3666d30f41e01f976594816
kkamdung <kkamdung@empal.com>
2025-03-14 21:22:35 +0900

    쉘 스크립트 수정

--- kubernetes/platform/development/create-cluster.sh
+++ kubernetes/platform/development/create-cluster.sh
@@ -12,50 +12,62 @@ minikube addons enable ingress --profile polar
 
 sleep 30
 
-echo "\n📦 Deploying Keycloak...\n"
+echo "\n📦 Deploying Keycloak..."
 
 kubectl apply -f services/keycloak-config.yml
 kubectl apply -f services/keycloak.yml
 
 sleep 5
 
-echo "\n⌛ Waiting for Keycloak to be deployed...\n"
+echo "\n⌛ Waiting for Keycloak to be deployed..."
 
 while [ $(kubectl get pod -l app=polar-keycloak | wc -l) -eq 0 ] ; do
   sleep 5
 done
 
+echo "\n⌛ Waiting for Keycloak to be ready..."
+
 kubectl wait \
   --for=condition=ready pod \
-  --selector=db=polar-keycloak \
-  --timeout=180s
+  --selector=app=polar-keycloak \
+  --timeout=300s
 
+echo "\n⌛ Ensuring Keycloak Ingress is created..."
 
-echo "\n📦 Deploying platform services...\n"
-kubectl apply -f services
+kubectl apply -f services/keycloak.yml
+
+echo "\n📦 Deploying PostgreSQL..."
+
+kubectl apply -f services/postgresql.yml
 
 sleep 5
 
-echo "\n⌛ Waiting for PostgreSQL to be deployed...\n"
+echo "\n⌛ Waiting for PostgreSQL to be deployed..."
 
 while [ $(kubectl get pod -l db=polar-postgres | wc -l) -eq 0 ] ; do
   sleep 5
 done
 
-echo "\n⌛ Waiting for PostgreSQL to be ready...\n"
+echo "\n⌛ Waiting for PostgreSQL to be ready..."
 
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=polar-postgres \
   --timeout=180s
 
-echo "\n⌛ Waiting for Redis to be deployed...\n"
+echo "\n📦 Deploying Redis..."
+
+kubectl apply -f services/redis.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for Redis to be deployed..."
 
 while [ $(kubectl get pod -l db=polar-redis | wc -l) -eq 0 ] ; do
   sleep 5
 done
 
-echo "\n⌛ Waiting for Redis to be ready...\n"
+echo "\n⌛ Waiting for Redis to be ready..."
 
 kubectl wait \
   --for=condition=ready pod \
@@ -68,32 +80,36 @@ kubectl apply -f services/rabbitmq.yml
 
 sleep 5
 
-echo "\n⌛ Waiting for RabbitMQ to be deployed...\n"
+echo "\n⌛ Waiting for RabbitMQ to be deployed..."
 
 while [ $(kubectl get pod -l db=polar-rabbitmq | wc -l) -eq 0 ] ; do
   sleep 5
 done
+
+echo "\n⌛ Waiting for RabbitMQ to be ready..."
 
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=polar-rabbitmq \
   --timeout=180s
 
-echo "\n📦 Deploying Polar UI...\n"
+echo "\n📦 Deploying Polar UI..."
 
 kubectl apply -f services/polar-ui.yml
 
 sleep 5
 
-echo "\n⌛ Waiting for Polar UI to be deployed...\n"
+echo "\n⌛ Waiting for Polar UI to be deployed..."
 
 while [ $(kubectl get pod -l app=polar-ui | wc -l) -eq 0 ] ; do
   sleep 5
 done
 
+echo "\n⌛ Waiting for Polar UI to be ready..."
+
 kubectl wait \
   --for=condition=ready pod \
-  --selector=app=polar-ui
+  --selector=app=polar-ui \
   --timeout=180s
 
 echo "\n⛵ Happy Sailing!\n"
